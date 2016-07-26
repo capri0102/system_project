20bf67791634beec67b8ca5ef8ae570b81cb94d0
capri0102 <capri9201@gmail.com>
2016-07-26 12:19:27 +0900

    newgit status!

--- file_slice.c
+++ file_slice.c
@@ -3,17 +3,14 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ftw.h>
+#include <errno.h>
 
-#define PERMS 0644
+#define PERMS 0777
 #define DEFNAME "temp"
 #define DEFSIZE 256	
-
-struct f_slice{
-	char *m_in;
-	char *m_out;
-	char *m_dir;
-	int m_size;
-};
 
 
 /******************** option *********************
@@ -23,14 +20,19 @@ struct f_slice{
  -d : story directory option if ignore directory make directory, if not input
       excution directory
 ***************************************************/
-
 void print_help();
-int file_silce(struct f_slice* m_file);
+int list(const char *name, const struct stat *status, int type);
+int dir_make(char* m_dir);
+int file_slice(char* m_in, char* m_out, char* m_dir, int m_size);
+
+static  int check_dir = 0;
+static char* dir = NULL;
 int main(int argc, char **argv){
-	int i, ch;
-	struct f_slice f_s;
-	f_s.m_size = DEFSIZE;
-	strcpy(f_s.m_out, DEFNAME);
+	int i, ch, size;
+	char *in = NULL, *out = DEFNAME;
+	
+	size = DEFSIZE;
+
 	if(argc < 3 || argc > 9 || argc%2 == 0){
 		print_help();
 		exit(1);
@@ -38,23 +40,28 @@ int main(int argc, char **argv){
 	while((ch = getopt(argc, argv, "n:s:o:d:")) != -1){
 		switch(ch){
 		case 'n':
-			strcpy(f_s.m_out, optarg);
+			out = optarg;
 			break;
 		case 's':
-			f_s.m_size = atoi(optarg);
+			size = atoi(optarg);
 			break;
 		case 'o':
-			strcpy(f_s.m_in, optarg);
+			in = optarg;
 			break;
 		case 'd':
-			strcpy(f_s.m_dir, optarg);
+			check_dir = 1;
+			dir = optarg;
 			break;
 		default:
 			print_help();
 		}
 	}
-	if(strlen(f_s.m_in) < 1)
+
+	if(!in)
 		print_help();
+
+	if(file_slice(in, out, dir, size) == 0)
+		fprintf(stdout, "Complete...\n");
 
 	return 0;
 }
@@ -68,20 +75,63 @@ void print_help(){
 	fprintf(stdout, " -d\tstory directory\n");
 	exit(1);
 }
-
-int file_silce(struct f_slice* m_file){
+int list(const char *name, const struct stat *status, int type){
+	return 0;
+}
+int dir_make(char* m_dir){
+	int retval;
+	int err;
+	retval = mkdir(m_dir, 0777);
+	if(retval == 0)
+		return 0;
+	else if(retval == -1){
+		if(errno == EEXIST)
+			return 0;
+		else
+			return 1;
+	}	
+}
+int file_slice(char* m_in, char* m_out, char* m_dir, int m_size){
 	int in_file, out_file;
-	int i;
-	if((in_file = open(*m_file->m_in, O_RDONLY)) == -1){
+	int i = 0;
+	char* outname = (char *)malloc((strlen(m_out)+5));
+	char* buffer = (char *)malloc(m_size);
+	ssize_t n1, w1;
+	if(m_dir){
+		if(dir_make(m_dir) == 1){
+			fprintf(stderr, "directory make error!\n");
+			return 1;
+		}
+	}
+	if((in_file = open(m_in, O_RDONLY)) == -1){
 		fprintf(stderr, "file open error!\n");
+		free(outname);
+		free(buffer);
 		return 1;
 	}
+	while((n1 = read(in_file, buffer, m_size)) > 0){
+		if(check_dir == 0)
+			sprintf(outname, "%s_%d", m_out, i++);
+		else
+			sprintf(outname, "./%s/%s_%d", m_dir, m_out, i++);
 
-	if((out_file = open(*m_file->m_out, O_WRONLY|O_CREAT|O_TRUNC)) == -1){
-		fprintf(stderr, "fileopen error!\n");
-		return 1;
+		printf("Result File name : %s\n", outname);
+		if((out_file = open(outname, O_WRONLY|O_CREAT|O_TRUNC, PERMS)) == -1){
+			fprintf(stderr, "fileopen error!\n");
+			free(outname);
+			free(buffer);
+			return 1;
+		}
+		if(write(out_file, buffer, n1) < n1){
+			fprintf(stderr, "filewrite error!\n");
+			free(outname);
+			free(buffer);
+			return 1;
+		}
+		close(out_file);
 	}
-
-	
+	close(in_file);
+	free(outname);
+	free(buffer);
 	return 0;
 }
